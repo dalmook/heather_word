c2bb88e2b1cf2a79a43940be310cf606da3c401a
dalmook <94118801+dalmook@users.noreply.github.com>
2026-05-11 23:57:04 +0900

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -2,7 +2,7 @@
 // Firebase를 아직 안 쓸 때는 이 파일을 그대로 두면 LOCAL 모드로 동작합니다.
 // Firebase Console에서 Web app 설정값을 만든 뒤 아래 USE_FIREBASE를 true로 바꾸세요.
 
-window.HEATHER_USE_FIREBASE = false;
+window.HEATHER_USE_FIREBASE = true;
 
 // 같은 단어장을 공유할 그룹 ID입니다.
 // 가족끼리 쓰면 "family-main", 반별로 쓰면 "class-1a"처럼 바꿔도 됩니다.
